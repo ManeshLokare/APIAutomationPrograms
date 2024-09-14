aad3544dc13d5cd1a41f279fbb1aadd460de31e7
ManeshLokare <maneshlokare123@gmail.com>
2024-09-14 22:03:35 +0530

    New Added

---
+++ src/test/java/org/example/Test_002.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import io.restassured.RestAssured;
+
+public class Test_002 {
+    public static void main(String[] args) {
+        System.out.println("Rest Assured Test Case");
+        System.out.println("GET Request Demo");
+
+        // Gherkins syntax....>
+        // given() --> URL , Headers, Payload or Body
+        // when () --> HTTP Methods  (get,post,patch,put,delete)
+        // then () --> verify response (ER==AR)
+
+        RestAssured
+                .given().baseUri("https://reqres.in/api")
+                .basePath("/users/2").log().all()
+                .when()
+                     .get()
+                .then().log().all()
+                .statusCode(200);
+
+
+
+    }
+}
